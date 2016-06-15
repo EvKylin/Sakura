f06788104daf2bf2525b07b707786ece53292969
Kylin Han <Kylin Han>
2016-06-15 17:19:39 +0800

    Initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name sakuraApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the sakuraApp
+ */
+angular.module('sakuraApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name sakuraApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the sakuraApp
+ */
+angular.module('sakuraApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
